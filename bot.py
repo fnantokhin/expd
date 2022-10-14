81e56c4cce20bc5f5801038e827c42f82bcdfe18
Fyodor <uroboros5144@gmail.com>
2022-10-14 12:27:46 +0300

    Implement /update x(int)
    Bot is now able to automatically send current_snapshot.jpg every x seconds without any
    realtime user input. Using job_queue and related code organisation practice as shown in
    https://github.com/python-telegram-bot/python-telegram-bot/blob/master/examples/timerbot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-async def callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Block bot for non AUTHENTICATED USERS"""
     if update.effective_user.id in custom_config.SPECIAL_USERS:
         pass
@@ -58,27 +58,67 @@ async def callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # and context.
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) \
-      -> None:
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     # user = update.effective_user
     await update.message.reply_text("homewebcamssurveillance_9581_bot start()")
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) \
-      -> None:
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
     await update.message.reply_text(
         "Usage:\n"
         "/debug - send last snapshot made by motion "
         "program and last motion capture video recorded\n"
-        "/help - send this message")
+        "/help - send this message\n"
+        "/update x(int) - send last snapshot every x seconds")
 
 
-async def debug_command(update: Update, context: ContextTypes.DEFAULT_TYPE) \
-      -> None:
-    await update.message.reply_photo(open(custom_config.CURRENT_PHOTO, 'rb'))
+async def debug_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send debug information (last snapshot and last video as set
+    in custom_config.py"""
+    await update.message.reply_photo()
     await update.message.reply_video(open(custom_config.CURRENT_VIDEO, 'rb'))
+
+
+async def update_job(context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send current local snapshot."""
+    job = context.job
+    await context.bot.send_photo(job.chat_id,
+                                 photo=open(custom_config.CURRENT_PHOTO, 'rb'))
+
+
+def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+async def update_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Set interval for automatically sending current local snapshot every
+    x seconds or disable the feature"""
+    chat_id = update.effective_message.chat_id
+    try:
+        update_interval = int(context.args[0])
+    except Exception:
+        await update.message.reply_text("wrong arguments in /update x(int)")
+        return
+
+    if update_interval < 1:
+        await update.message.reply_text("updates disabled")
+        remove_job_if_exists(str(chat_id), context)
+    else:
+        await update.message.reply_text(
+            "updates enabled every " +
+            str(update_interval) + "s")
+        remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_repeating(
+            update_job, update_interval, chat_id=chat_id,
+            name=str(chat_id), data=update_interval)
 
 
 def main() -> None:
@@ -92,6 +132,7 @@ def main() -> None:
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("debug", debug_command))
+    application.add_handler(CommandHandler("update", update_command))
     application.add_handler(CommandHandler("help", help_command))
 
     # Run the bot until the user presses Ctrl-C
